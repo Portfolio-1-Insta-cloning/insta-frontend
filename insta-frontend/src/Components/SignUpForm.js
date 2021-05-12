7af39ff790aed5aadb45eed73497b9f24785afe0
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-05-11 20:39:16 -0400

    sign up form is ready

--- insta-frontend/src/Components/SignUpForm.js
+++ insta-frontend/src/Components/SignUpForm.js
@@ -1,15 +1,47 @@
 import React, { useState } from "react";
-// import { Button, Form, FormGroup, Lable, Input, Container, Row, Col } from "reacstrap";
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from "reactstrap";
+import axios from "axios"
 
 const SignUpForm = (props) => {
+
     const [regForm, setRegForm] = useState({
         username: "",
         password: ""
     });
+
+    const inputchange = (e) => {
+        setRegForm({ ...regForm, [e.target.name]: e.target.value });
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:5000/api/users/signup/", regForm)
+            .then((res) => {
+                props.history.push('/')
+            })
+            .catch(err => console.log("Signup error", err))
+    };
+
     return (
-        <div>
-            <p> Sign Up Form</p>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <h3>Sign Up</h3>
+                    <Form onSubmit = {submitHandler}>
+                        <FormGroup>
+                            <Label for="username">Username</Label>
+                            <Input type="text" name="username" id="username" placeholder="Enter your name" onChange={ inputchange }/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="password">Password</Label>
+                            <Input type = "password" name = "password" id = "password" placeholder = "Enter your password" onChange={ inputchange }/>
+                        </FormGroup>
+                        <Button>Signup</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
